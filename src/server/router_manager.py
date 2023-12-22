7150beecdae5cbe67e29f9c56abc147fcab8684c
Danil <danilpugachev253@gmail.com>
2023-12-22 22:19:51 +0300

    ага

--- src/server/router_manager.py
+++ src/server/router_manager.py
@@ -1,5 +1,5 @@
 import fastapi
-import resolver_manager
+from resolver_manager import ResolverManager
 from typing import Type
 from src.server.database.peewee_models import BaseModel
 from src.server.database.pydantic_models import ModifyBaseModel
@@ -11,10 +11,11 @@ class RouterManager:
         self.pydantic_model = pydantic_models
         self.peewee_model = peewee_model
         self.fastapi_router: fastapi.APIRouter = fastapi.APIRouter(prefix=prefix, tags=tags)
-        self.resolver_manager = ResolverManager(self.peewee_model,self.pydantic_model)
+        self.resolver_manager = ResolverManager(self.peewee_model, self.pydantic_model)
         self.__init_routers()
 
     def __init_routers(self):
+        pm = self.pydantic_model
 
         @self.fastapi_router.get(path='/get/{id}', response_model=dict)
         def get_record(id: int) -> dict:
@@ -26,14 +27,14 @@ class RouterManager:
         
 
         @self.fastapi_router.post(path='/new', response_model=dict)
-        def new(new_model: self.pydantic_model) -> dict:
-            return self.resolver_manager.new(new_model = new_model)
+        def new(new_model: pm) -> dict:
+            return self.resolver_manager.new(new_model=new_model)
         
         @self.fastapi_router.put(path='/update/{id}', response_model=dict)
-        def update(id: int, new_model: self.pydantic_model) -> dict:
+        def update(id: int, new_model: pm) -> dict:
             return self.resolver_manager.update(id, new_model)
         
         @self.fastapi_router.delete(path='/delete/{id}')
         def delete(id: int):
-            return self.resolver_manager.delete(id = id)
+            return self.resolver_manager.delete(id=id)
         
---
+++ src/server/routers.py
@@ -0,0 +1,8 @@
+from src.server.database import peewee_models
+from src.server.database import pydantic_models
+from src.server.router_manager import RouterManager
+
+
+
+
+
